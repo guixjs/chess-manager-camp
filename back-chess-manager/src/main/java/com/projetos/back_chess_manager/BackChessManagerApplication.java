f19d0ec14d67e31fc9ca940805b93e4ae3fa9f48
Guilherme Felix <f3lixguilherme@gmail.com>
2025-07-19 15:55:00 -0300

    firts commit

---
+++ back-chess-manager/src/main/java/com/projetos/back_chess_manager/BackChessManagerApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.back_chess_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackChessManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackChessManagerApplication.class, args);
+	}
+
+}
---
+++ back-chess-manager/src/test/java/com/projetos/back_chess_arena/BackChessArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.back_chess_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackChessArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
